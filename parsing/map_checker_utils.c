6e1dc6edca36f3279e2bbc7e0a1295f73f547bdd
Mohammed Benkhattab <mbenkhat@e3r8p10.1337.ma>
2022-07-30 18:10:32 +0100

    parsing not done yet

---
+++ parsing/map_checker_utils.c
@@ -0,0 +1,42 @@
+#include "parsing.h"
+
+int	check_map_characters(char **map)
+{
+	int	i;
+	int	j;
+	int	p;
+
+	i = 0;
+	p = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W')
+				p++;
+			else if (!is_meta(map[i][j]) && map[i][j] != '1' && map[i][j] != ' ')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	if (p != 1)
+		return (1);
+	return (0);
+}
+
+int	all_set(data *d)
+{
+	if (!d->n_path || !d->s_path || !d->w_path
+		|| !d->e_path || !d->c_hexa || !d->f_hexa
+		|| !d->map)
+	{
+		return 1;
+	}
+	if (check_map_characters(d->map))
+	{
+		return 1;
+	}
+	return 0;
+}
---
+++ parsing/parsing_utils.c
@@ -0,0 +1,83 @@
+#include "parsing.h"
+
+void	print_error(int error)
+{
+	printf("Error\n");
+	if (error == 9)
+		printf("Map not found!\n");
+	else if (error == 10)
+		printf("Invalid file extension!\n");
+	else if (error == 11)
+		printf("Unown map element!\n");
+	else if (error == 12)
+		printf("Color Parsing Error!\n");
+	else if (error == 13)
+		printf("Unown map character!\n");
+	else if (error == 14)
+		printf("Map element messing!\n");
+	else
+		printf("%s\n", strerror(error));
+	exit(1);
+}
+
+int	is_map(char *str)
+{
+	int	len;
+
+	if (!str)
+		return 1;
+	len = ft_strlen(str);
+	while (*str == '1' || *str == ' ')
+		str++;
+	if (!(*str) && len)
+		return (1);
+	return (0);
+}
+
+unsigned long	rgb_to_hexa(int r, int g, int b)
+{   
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
+int	arrlen(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		i++;
+	return (i);
+}
+
+void	free_arr(char **arr)
+{
+	int	i;
+
+	i = 0;
+	if(arr)
+		while (arr[i])
+		{
+			free(arr[i]);
+			arr[i] = 0;
+			i++;
+		}
+	free(arr);
+}
+
+void	free_data(data *d)
+{
+	if (d->n_path)
+		free(d->n_path);
+	if (d->e_path)
+		free(d->e_path);
+	if (d->w_path)
+		free(d->w_path);
+	if (d->s_path)
+		free(d->s_path);
+	d->n_path = 0;
+	d->e_path = 0;
+	d->w_path = 0;
+	d->s_path = 0;
+	free_arr(d->map);
+	free(d);
+}
